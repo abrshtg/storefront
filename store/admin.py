41b38d5a1914c96571ba22c772f40a12582c0513
abrshtg <abrshtgam@gmail.com>
2024-10-06 14:03:16 +0300

    register 'Customer' model to admin page

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from store.models import Customer
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ["first_name", "last_name", "membership"]
+    list_editable = ["membership"]
+    ordering = ["first_name", "last_name"]
+    list_per_page = 10
