e18d682e2423b30d575094f2f4c700bf4eaa77b2
abrshtg <abrshtgam@gmail.com>
2024-10-06 18:05:29 +0300

    add custom filter for product admin page on inventory

--- store/admin.py
+++ store/admin.py
@@ -7,6 +7,20 @@ from django.utils.http import urlencode
 from store.models import Customer, Product, Order, Collection
 
 
+class InventoryFilter(admin.SimpleListFilter):
+    title = "inventory"
+    parameter_name = "inventory"
+
+    lt_ten = "<10"
+
+    def lookups(self, request, model_admin):
+        return [(self.lt_ten, "Low")]
+
+    def queryset(self, request, queryset):
+        if self.value() == self.lt_ten:
+            return queryset.filter(inventory__lt=10)
+
+
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ["first_name", "last_name", "membership", "order_count"]
@@ -29,7 +43,7 @@ class CustomerAdmin(admin.ModelAdmin):
 class ProductAdmin(admin.ModelAdmin):
     list_display = ["title", "unit_price", "inventory_status", "collection_title"]
     list_editable = ["unit_price"]
-    list_filter = ["collection", "last_update"]
+    list_filter = ["collection", "last_update", InventoryFilter]
     list_per_page = 10
     list_select_related = [
         "collection"
