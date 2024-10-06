6144c019a19e855489cca30e760fc5b588181ef1
abrshtg <abrshtgam@gmail.com>
2024-10-07 01:43:42 +0300

    update product inventory to positive integer field

--- store/models.py
+++ store/models.py
@@ -25,7 +25,7 @@ class Product(models.Model):
     unit_price = models.DecimalField(
         max_digits=6, decimal_places=2, validators=[MinValueValidator(1)]
     )
-    inventory = models.IntegerField()
+    inventory = models.PositiveIntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
     promotions = models.ManyToManyField(Promotion)
