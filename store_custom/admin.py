f4b2a90df2fd5c661aa44d7b8ea26327b6c00bb1
abrshtg <abrshtgam@gmail.com>
2024-10-09 22:53:15 +0300

    decouple store and tag app using store_custom app

--- store_custom/admin.py
+++ store_custom/admin.py
@@ -1,1 +1,19 @@
-# Register your models here.
+from django.contrib import admin
+from django.contrib.contenttypes.admin import GenericTabularInline
+
+from store.admin import ProductAdmin
+from store.models import Product
+from tags.models import TaggedItem
+
+
+class TaggedItemInline(GenericTabularInline):
+    model = TaggedItem
+    autocomplete_fields = ["tag"]
+
+
+class CustomProductAdmin(ProductAdmin):
+    inlines = [TaggedItemInline]
+
+
+admin.site.unregister(Product)
+admin.site.register(Product, CustomProductAdmin)
